2f692a5edb524d1038410df07d1166b8b67e8985
RolandoCM <castillor493@gmail.com>
2017-10-27 00:48:24 -0500

    nuevo cluster

--- CariesRobot/Main.py
+++ CariesRobot/Main.py
@@ -1,15 +1,22 @@
+import time
+import logger.logs
+
 from comunicacion.TransformarCoordenadas import TransformarCoordenadas
+from procesarImagen.Camara import Camara
 from procesarImagen.Coordenadas import Coordenadas
+from procesarImagen.RemoverFondo import RemoverFondo
+from procesarImagen.kmeansImage import kmeans
 
 # modulo de captura de imagen
-#Camara().Cam()
-#time.sleep(5)
+Camara().Cam()
+time.sleep(5)
 
 #modulo para Remover el fondo de la Imagen
-#RemoverFondo().remove()
+
+RemoverFondo().remove()
 
 #Clousterizar la Imagen kmeans/kmeansImage.py
-#KmeansImage().cluster()
+kmeans().identificarCarie()
 
 # optener las coordenadas para enviar por modulo de comunicacion
 coordenadaX, coordenadaY=Coordenadas().optener()
